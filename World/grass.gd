e96f22666dac6fdafcde300f2977dfef31f336fa
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-02-07 08:21:58 +0900

    modified:   World/grass.gd

--- World/grass.gd
+++ World/grass.gd
@@ -1,10 +1,9 @@
 extends Node2D
 
-# @onready var GrassEffect = load("res://Effects/grass_effect.tscn")
+@onready var GrassEffect = load("res://Effects/grass_effect.tscn")
 
-func _process(delta):
+func _process(_delta):
 	if Input.is_action_just_pressed("attack"):
-		var GrassEffect = load("res://Effects/grass_effect.tscn")
 		var grassEffect = GrassEffect.instantiate()
 		var world = get_tree().current_scene
 		world.add_child(grassEffect)
