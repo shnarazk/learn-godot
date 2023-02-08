a8afbcd28d217001a3f4c5b36a17ffeca93a1948
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-02-09 00:02:13 +0900

    part 18 done

--- UI/health_ui.gd
+++ UI/health_ui.gd
@@ -3,17 +3,27 @@ extends Control
 @export
 var stats: Node
 
+@onready
+var label = $Label
+@onready
+var heartEmpty = $HeartEmpty
+@onready
+var heartFull = $HeartFull
+
 var hearts: int = 4
 var max_hearts: int = 4
 
 func set_hearts(value) -> void:
 	hearts = min(max_hearts, value)
-
+	# label.text = "HP: " + str(hearts)
+	heartFull.size.x = hearts * 15
+	
 func _ready():
 	max_hearts = stats.max_health
+	heartEmpty.size.x = max_hearts * 15
 	hearts = max_hearts
 #	playerStats.connect("health_changed", self.set_hearts)
 
-func _on_player_stats_health_changed(value):
+func _on_player_stats_health_changed(value) -> void:
 	set_hearts(value)
 	print(hearts)
