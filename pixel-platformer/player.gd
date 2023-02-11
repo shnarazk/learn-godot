e79e0e5e6062ccac187563929a3966547d095b89
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-02-11 11:01:09 +0900

    pixel-platformer: part 2 done IMHO

--- pixel-platformer/player.gd
+++ pixel-platformer/player.gd
@@ -1,28 +1,32 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export
+var SPEED        : float =  40.0
+@export
+var JUMP_VELOCITY: float = 400.0
+@export
+var AIR_BREAK    : float =   0.25
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-
-func _physics_process(delta):
+func _physics_process(delta) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_accept"):
+			velocity.y = -JUMP_VELOCITY
+	else:
+		if Input.is_action_just_released("ui_accept") and velocity.y < -10.0:
+			velocity.y *= AIR_BREAK
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction: float = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
 	move_and_slide()
