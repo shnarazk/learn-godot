2f474d9d28bcbce478821dc75037d8240efc8893
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-06-01 13:09:02 +0900

    version 2, no more grouping Control

--- await-dialog/select_dialog.gd
+++ await-dialog/select_dialog.gd
@@ -1,4 +1,4 @@
-extends Control
+extends Node2D
 signal selected(value)
 
 
--- await-dialog/toplevel.gd
+++ await-dialog/toplevel.gd
@@ -5,10 +5,10 @@ func _ready():
 	var loop = true
 	while loop:
 		loop = false
-		var res = await $SelectorDialog.open("分かれ道です", "👈左に行く", "右に行く👉")
+		var res = await $SelectDialog.open("分かれ道です", "👈左に行く", "右に行く👉")
 		if res == 1:
-			res = await $SelectorDialog.open("賑やかな場所に近づいてきました", "左町に入る", "左町の入口で一休みする")
+			res = await $SelectDialog.open("賑やかな場所に近づいてきました", "左町に入る", "左町の入口で一休みする")
 		else:
-			res = await $SelectorDialog.open("[color=red]怪しい雰囲気の場所に出ました[/color]", "引き返す", "様子を伺う")
+			res = await $SelectDialog.open("[color=red]怪しい雰囲気の場所に出ました[/color]", "引き返す", "様子を伺う")
 			print(res)
 			loop = res == 1
